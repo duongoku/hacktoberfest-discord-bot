e241b49adb61e76d5fb22dfb66f4198f16a6ff00
duongoku <nguyenvubinhduong@gmail.com>
2018-10-01 21:10:21 +0700

    Formatting

--- index.js
+++ index.js
@@ -5,31 +5,34 @@ const prefix = 'hack!';
 client.on('ready', () => console.log('Bot has logged in!'));
 
 client.on('message', async (msg) => {
-  if(msg.author.bot || !msg.content.startsWith(prefix)) return;
-  let args = msg.content.split(' ');
-  const command = args.shift().slice(prefix.length);
-  //Add your commands here. Good command handlers are overrated :POGGERS:
-  
-  switch(command){
+	if(msg.author.bot || !msg.content.startsWith(prefix)) return;
+	let args = msg.content.split(' ');
+	const command = args.shift().slice(prefix.length);
+	//Add your commands here. Good command handlers are overrated :POGGERS:
 
-  	case "ping":
-    	const m = await msg.channel.send('Pinging...');
-    	m.edit(`Ponggers! Client ping: \`${m.createdTimestamp - msg.createdTimestamp}\`ms. Heartbeat ping: \`${client.ping}\`ms`);\
-    	break;
+	switch(command){
 
-    case "info":
-    	const embed = new Discord.RichEmbed()
+		case "ping":
+
+			const m = await msg.channel.send('Pinging...');
+			m.edit(`Ponggers! Client ping: \`${m.createdTimestamp - msg.createdTimestamp}\`ms. Heartbeat ping: \`${client.ping}\`ms`);\
+			
+			break;
+
+		case "info":
+
+			const embed = new Discord.RichEmbed()
 			.setColor("BLUE")
 			.setTitle("Detailed Informations")
 			.addBlankField(true)
 			.addBlankField(true)
 			.setImage("https://pbs.twimg.com/profile_images/1042878305874128896/McCIWyzX_400x400.jpg")
 			.addField("Bot's name","hacktoberfest-discord-bot",true)
-		  message.channel.send(embed);
-		break;
+			message.channel.send(embed);
+			
+			break;
 
-  }
-  
+	}
 })
 
 client.login(process.env.TOKEN);
